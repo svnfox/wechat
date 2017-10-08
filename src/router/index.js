76c687e62d2e98d137f2009b9344244ca20d7df0
Kilemi <1014751893@qq.com>
2017-10-08 10:32:39 +0800

    adjust config

--- src/router/index.js
+++ src/router/index.js
@@ -5,34 +5,33 @@ import routes from './routes'
 Vue.use(Router)
 
 //跳转后滚到顶部
-const scrollBehavior = (to, from, savedPosition)=> {
+const scrollBehavior = (to, from, savedPosition) => {
   return { x: 0, y: 0 }
 }
 
 const router = new Router({
-  routes: routes,
+  routes,
   scrollBehavior
 })
 
-// router.beforeEach((to, from, next) => {
-// 	if (to.matched.some(record => record.meta.requiresAuth)) {
-// 	  // this route requires auth, check if logged in
-// 	  // if not, redirect to login page.
-// 	  if (!auth.loggedIn()) {
-// 	    next({
-// 	      path: '/login',
-// 	      query: { redirect: to.fullPath }
-// 	    })
-// 	  } else {
-// 	    next()
-// 	  }
-// 	} else {
-// 	  next() // 确保一定要调用 next()
-// 	}
-// })
-//
+router.beforeEach((to, from, next) => {
+  console.log(to)
+	if (to.matched.some(record => record.meta.auth)) {
+	  // if (!auth.loggedIn()) {
+	  //   next({
+	  //     path: '/login',
+	  //     query: { redirect: to.fullPath }
+	  //   })
+	  // } else {
+	    next()
+	  // }
+	} else {
+	  next() // 确保一定要调用 next()
+	}
+})
+
 router.afterEach(router => {
-  
+
 })
 
 export default router
